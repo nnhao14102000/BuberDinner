1c93254a43f32ce33d4603f99f7443c10d01b9fd
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-07-17 09:34:01 +0700

    Setup Authentication

---
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        
+        return services;
+    }
+}
