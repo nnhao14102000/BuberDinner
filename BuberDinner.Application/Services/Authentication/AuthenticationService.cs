5f91a6f63e48ffafe8e2764745bf0ab1323e1bc0
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-07-17 14:32:25 +0700

    Generate Token and using Secrets

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,8 +1,17 @@
-﻿namespace BuberDinner.Application.Services.Authentication;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
 public class AuthenticationService : IAuthenticationService
 {
-    public AuthenticationResult Login(string email, string password)
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
     {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    {       
         return new AuthenticationResult(
             Guid.NewGuid(),
             "firstName",
@@ -13,12 +22,20 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
+        // Check if user already exists
+
+        // Create user (generate unique ID)
+
+        // Create JWT token
+        Guid userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
         return new AuthenticationResult(
             Guid.NewGuid(),
             firstName,
             lastName,
             email,
-            "token");
+            token);
     }
 }
 
--- Docs/Api.md
+++ Docs/Api.md
@@ -8,6 +8,11 @@
     - [Login](#login)
       - [Login Request](#login-request)
       - [Login Response](#login-response)
+    - [Setting Secret](#setting-secret)
+      - [Notice](#notice)
+      - [Init](#init)
+      - [Set](#set)
+      - [List](#list)
 
 ## Auth
 
@@ -60,3 +65,45 @@ POST {{host}}/auth/login
 ```
 
 #### Login Response
+
+```js
+200 OK
+```
+
+```json
+{
+    "id": "d89c2d9a-eb2e-4075-95ff-b92920b55aa104",
+    "firstName": "Amichai",
+    "lastName": "Mantinband",
+    "email": "amichai@mantinband.com",
+    "token": "eyJhb..z9dqcnXoY"
+}
+```
+
+### Setting Secret
+
+#### Notice
+
+```js
+BuberDinner.Api.csproj 
+...
+7 <UserSecretsId>a4f2421f-f23d-4515-baaf-22b03eaf0bda</UserSecretsId>
+...
+```
+#### Init
+
+```js
+dotnet user-secrets init --project .\BuberDinner.Api\
+```
+
+#### Set
+
+```js
+dotnet user-secrets set --project .\BuberDinner.Api\ "JwtSettings:Secret" "supe-secret-key-from-user-secrets"
+```
+
+#### List
+
+```js
+dotnet user-secrets list --project .\BuberDinner.Api\
+```
